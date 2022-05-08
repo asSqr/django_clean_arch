e72cf790b2ba8b46446110b92a289d2bfc2c8a71
Ysmr-Ry <ryo.yossy@live.jp>
2022-05-08 16:47:53 +0900

    fix: abstractmethod

--- gadget_app/adapters/repositories/base_repository.py
+++ gadget_app/adapters/repositories/base_repository.py
@@ -1,15 +1,18 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Dict, Any, Optional
 
 from gadget_app.models import BaseModel
 
 
 class BaseRepository(ABC):
+    @abstractmethod
     def __init__(self, model_class: BaseModel):
         raise NotImplementedError()
     
+    @abstractmethod
     def get(self, id: str) -> Optional[BaseModel]:
         raise NotImplementedError()
 
+    @abstractmethod
     def create(self, data: Dict[str, Any]) -> str:
         raise NotImplementedError()
--- gadget_app/response_objects/base_response.py
+++ gadget_app/response_objects/base_response.py
@@ -1,9 +1,10 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Dict, Any
 
 
 @dataclass
 class BaseResponse(ABC):
+    @abstractmethod
     def to_json(self) -> Dict[str, Any]:
         raise NotImplementedError()
